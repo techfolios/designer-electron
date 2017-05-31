f61174b97ed4ece205395de1b3246d4d9d039840
Tyler Chong <tylrchong@gmail.com>
2017-05-30 15:44:46 -1000

    Fuck it, Ship it

--- renderer.js
+++ renderer.js
@@ -5,14 +5,11 @@ function searchToObject() {
     obj = {},
     pair,
     i;
-
-  for ( i in pairs ) {
-    if ( pairs[i] === "" ) continue;
-
+  for (i in pairs) {
+    if (pairs[i] === "") continue;
     pair = pairs[i].split("=");
-    obj[ decodeURIComponent( pair[0] ) ] = decodeURIComponent( pair[1] );
+    obj[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
   }
-
   return obj;
 }
 
@@ -25,7 +22,7 @@ $.ajaxSetup({
 
 $.getJSON('https://api.github.com/user/repos', function (data) {
   data.forEach(function(e, i, a) {
-    if (i == 0) console.log(e);
+    // if (i == 0) console.log(e);
     const box = `
     <div class="ui raised very padded text container segment">
       <h2 class="ui header"><a href="#">${e.full_name}</a></h2>
@@ -33,8 +30,4 @@ $.getJSON('https://api.github.com/user/repos', function (data) {
     </div>`;
     $('#repos').append(box);
   });
-});
-
-$('#search').keydown(function() {
-  console.log($(this).val());
 });
