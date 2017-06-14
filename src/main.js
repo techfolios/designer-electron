f7b5cf5979ca0b1b6a643da620098b47d6dec0f0
Adam <adam359@hawaii.edu>
2017-06-14 00:57:51 -1000

    swiped from tyler, can authenticate

---
+++ src/main.js
@@ -0,0 +1,75 @@
+const electronOauth2 = require('electron-oauth2');
+const electron = require('electron');
+const path = require('path');
+const url = require('url');
+
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow = {};
+let token = {};
+
+var config = require('./config.js');
+
+function createWindow () {
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+  //mainWindow.webContents.openDevTools();
+  mainWindow.loadURL(`file://${__dirname}/index.html?access_token=${token.access_token}&token_type=${token.token_type}&scope=${token.scope}`);
+};
+
+app.on('ready', () => {
+  createWindow();
+
+  const windowParams = {
+    alwaysOnTop: false,
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: false
+    }
+  };
+
+  let scopes = ["public_repo"];
+
+  const options = {
+    scope: scopes.join(" "),
+    accessType: 'online'
+  };
+
+  const myApiOauth = electronOauth2(config, windowParams);
+
+  myApiOauth.getAccessToken(options)
+  .then((res) => {
+    token = res;
+    myApiOauth.refreshToken(token.refresh_token)
+    .then(newT => {
+      //use your new token
+      token = newT;
+    });
+  }, (rej) => {
+    console.log("Error retriving access token: " + rej);
+  });
+});
+
+app.on('window-all-closed', function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
