05a33cf3ddf91eb0372e85c9190ccb323507134a
Adam359 <adam359@hawaii.edu>
2017-06-17 14:10:49 -1000

    borrowing code from tyler's repo

---
+++ src/components/Root.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Main from './Main.jsx'
+
+class Root extends React.Component {
+  render() {
+    return (
+      <Main></Main> 
+    );
+  }
+}
+
+export default Root;
--- src/main.js
+++ src/main.js
@@ -1,75 +1,78 @@
-const electronOauth2 = require('electron-oauth2');
-const electron = require('electron');
-const path = require('path');
-const url = require('url');
+const electron = require('electron')
+const electronOauth2 = require('electron-oauth2')
+// Module to control application life.
+const app = electron.app
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow
 
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+const path = require('path')
+const url = require('url')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow = {};
-let token = {};
+let mainWindow
+let token
 
-var config = require('./config.js');
+var config = require('./config.js')
 
 function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow.loadURL(`file://${__dirname}/index.html?access_token=${token.access_token}&token_type=${token.token_type}&scope=${token.scope}`);
+  // mainWindow.webContents.openDevTools()
   mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null;
+    mainWindow = null
   });
-  //mainWindow.webContents.openDevTools();
-  mainWindow.loadURL(`file://${__dirname}/index.html?access_token=${token.access_token}&token_type=${token.token_type}&scope=${token.scope}`);
-};
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
 
 app.on('ready', () => {
-  createWindow();
 
+  createWindow();
   const windowParams = {
-    alwaysOnTop: false,
+    alwaysOnTop: true,
     autoHideMenuBar: true,
     webPreferences: {
       nodeIntegration: false
     }
-  };
-
-  let scopes = ["public_repo"];
+  }
+  let scopes = ["user", "notifications"];
 
   const options = {
     scope: scopes.join(" "),
     accessType: 'online'
-  };
+  }
 
   const myApiOauth = electronOauth2(config, windowParams);
 
   myApiOauth.getAccessToken(options)
-  .then((res) => {
-    token = res;
-    myApiOauth.refreshToken(token.refresh_token)
-    .then(newT => {
-      //use your new token
-      token = newT;
+    .then(t => {
+      token = t;
+      // myApiOauth.refreshToken(token.refresh_token)
+      //   .then(newT => {
+      //     //use your new token
+      //   });
     });
-  }, (rej) => {
-    console.log("Error retriving access token: " + rej);
-  });
 });
 
+// Quit when all windows are closed.
 app.on('window-all-closed', function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit();
+    app.quit()
   }
-});
+})
 
 app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
-    createWindow();
+    createWindow()
   }
-});
+})
