0af68f359a73311bde4d2f666ccb3c21f499cfda
Adam <adam359@hawaii.edu>
2017-07-04 19:35:29 -1000

    fixed incorrect variable name this.name to this.user.name

--- git.js
+++ git.js
@@ -12,37 +12,38 @@ class Git {
       url: {
         github: `https://github.com/${this.name}/`,
         techfolio: `https://github.com/${this.name}/${this.name}.github.io`,
-        local: OS.homedir() + "/.techfolios"
+        local: `${__dirname}/.techfolios` //OS.homedir() + "/.techfolios"
       }
     };
   }
 
   hasGithubIO() {
-    SimpleGit(this.user.url.local).listRemote(['--get-url'], function (err, data) {
-      if (!err) {
-        console.log('Remote url for repository at ' + __dirname + ':');
-        console.log(data);
-        return false;
-      }
-    });
+    SimpleGit(this.user.url.local)
+      .listRemote(['--get-url'], function (err, data) {
+        if (!err) {
+          console.log('Remote url for repository at ' + __dirname + ':');
+          console.log(data);
+          return false;
+        }
+      });
   }
 
   cloneTechfoliosTemplate() {
     let options = {};
     return SimpleGit(this.user.url.local)
-      .then(() => {
+      .exec(() => {
         console.log(`Cloning ${this.techfolios.url}...`);
       }).clone(this.techfolios.url, this.user.url.local, options, (err) => {
         if (err) {
           console.log(err);
           console.log(`Could not clone ${this.techfolios.url}`);
         }
-      }).then(() => {
-        console.log(`Adding remote ${this.name} ${this.user.url.techfolio}`);
-      }).addRemote(`${this.name}`, this.user.url.techfolio, (err) => {
+      }).exec(() => {
+        console.log(`Adding remote ${this.user.name} ${this.user.url.techfolio}`);
+      }).addRemote(`${this.user.name}`, this.user.url.techfolio, (err) => {
         if (err) {
           console.log(err);
-          console.log(`Could not add remote ${this.name} ${this.user.url.techfolio}`);
+          console.log(`Could not add remote ${this.user.name} ${this.user.url.techfolio}`);
         } else {
           console.log(`Cloned ${this.techfolios.url} to ${this.user.url.local}`);
         }
