252ca95de30f3245c81c74a478e98a45a463d004
Bill Mullen <wlmullen@gmail.com>
2017-07-19 20:45:51 -0600

    added secret.js

---
+++ app/src/secret.js
@@ -0,0 +1,8 @@
+module.exports = {
+  clientId: '2016eabf4bdeed559f74',
+  clientSecret: '5266450ff394ea50761dd08c6a005cc84a501862',
+  authorizationUrl: 'https://github.com/login/oauth/authorize',
+  tokenUrl: 'https://github.com/login/oauth/access_token',
+  useBasicAuthorizationHeader: false,
+  redirectUri: 'http://localhost'
+}
