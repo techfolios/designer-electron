50997e5a42a9526fa9a8a2e815d749c0d0ba68b7
SoraZodia <sorazodia@users.noreply.github.com>
2017-07-21 23:02:51 -1000

    Moved things to an different class, added icon

--- app/src/containers/essay/Essays.jsx
+++ app/src/containers/essay/Essays.jsx
@@ -1,45 +1,31 @@
 import React from 'react';
 import Path from 'path';
-import { Container, Card, Button } from 'semantic-ui-react';
+import { Container, Card, Button, Icon, Segment, Header } from 'semantic-ui-react';
 
 import FileCrawler from '../../utilities/file-crawler';
 import YAMLParser from '../../utilities/yaml-parser';
+import EssayList from './components/List.jsx';
 
 class Essay extends React.Component {
 
   constructor(props) {
     super(props);
-    this.directory = Path.resolve(props.dir, 'essays');
+    this.directory = props.dir;
     this.crawler = new FileCrawler(this.directory);
   }
 
-  getFiles() {
-    const list = [];
-    const parser = new YAMLParser(this.crawler.getFiles());
-
-    parser.read().forEach((data) => {
-      list.push(<Card key={data.attributes.title} color='blue'>
-        <Card.Content>
-          <Card.Header>
-            {data.attributes.title}
-          </Card.Header>
-        </Card.Content>
-        <Card.Content extra>
-          <div className='ui two buttons'>
-            <Button basic color='green'>Edit</Button>
-            <Button basic color='red'>Delete</Button>
-          </div>
-        </Card.Content>
-      </Card>);
-    });
-
-    return list;
+  getPage() {
+    return <EssayList dir={this.directory}/>;
   }
 
   render() {
     return <Container>
+      <Segment basic textAlign="center">
+        <Icon name="file text outline" size="huge" />
+        <Header as="h3"></Header>
+      </Segment>
       <Card.Group>
-        {this.getFiles()}
+        {this.getPage()}
       </Card.Group>
     </Container>;
   }
---
+++ app/src/containers/essay/components/List.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import Path from 'path';
+import { Card, Button } from 'semantic-ui-react';
+
+import FileCrawler from '../../../utilities/file-crawler';
+import YAMLParser from '../../../utilities/yaml-parser';
+
+class EssayList extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.directory = Path.resolve(props.dir, 'essays');
+    this.crawler = new FileCrawler(this.directory);
+  }
+
+  getFiles() {
+    const list = [];
+    const parser = new YAMLParser(this.crawler.getFiles());
+
+    parser.read().forEach((data) => {
+      list.push(<Card key={data.attributes.title} color='blue'>
+        <Card.Content>
+          <Card.Header>
+            {data.attributes.title}
+          </Card.Header>
+        </Card.Content>
+        <Card.Content extra>
+          <div className='ui two buttons'>
+            <Button basic color='green'>Edit</Button>
+            <Button basic color='red'>Delete</Button>
+          </div>
+        </Card.Content>
+      </Card>);
+    });
+
+    return list;
+  }
+
+  render() {
+    return <Card.Group>
+        {this.getFiles()}
+      </Card.Group>;
+  }
+}
+
+export default EssayList;
