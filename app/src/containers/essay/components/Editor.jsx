35ce1a86402df76c94217d5e5605756ed35711cd
SoraZodia <sorazodia@users.noreply.github.com>
2017-07-26 23:32:57 -1000

    Change Label field to use Accordian

--- app/src/containers/essay/components/Editor.jsx
+++ app/src/containers/essay/components/Editor.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Form, Button, Icon } from 'semantic-ui-react';
+import { Form, Button, Icon, Accordion } from 'semantic-ui-react';
 import Essay from '../Essays.jsx';
 import FileCrawler from '../../../utilities/file-crawler';
 import YAMLParser from '../../../utilities/yaml-parser';
@@ -17,6 +17,9 @@ class EssayEditor extends React.Component {
     this.crawler = new FileCrawler(Path.resolve(values.dir, 'essays'));
 
     this.save = this.save.bind(this);
+    this.displayLabels = this.displayLabels.bind(this);
+    this.addLabel = this.addLabel.bind(this);
+    this.removeLabel = this.removeLabel.bind(this);
   }
 
   static getDate() {
@@ -33,15 +36,55 @@ class EssayEditor extends React.Component {
     this.menu.setState(this.data);
   }
 
+  displayLabels() {
+    const list = [];
+    const labels = this.data.attributes.labels || [];
+
+    labels.forEach((value, index) => {
+      list.push(
+          <Form.Group key={`tags: ${value}`}>
+            <Form.Input defaultValue={value}
+                        onChange={ (event) => { labels[index] = event.target.value; } }/>
+            <Icon color='red' name='delete'
+                  onClick={ event => this.removeLabel(event, index)}/>
+          </Form.Group>);
+    });
+
+    return list;
+  }
+
+  removeLabel(event, value) {
+    event.preventDefault();
+    console.log(value);
+    this.remove('interests', 'name', value);
+  }
+
+  addLabel(event) {
+    event.preventDefault();
+    this.add(this.bio.interests, {
+      'name': '',
+      'keywords': []
+    });
+  }
+
   render() {
     const data = this.data;
     return <Form>
       <Form.Input label='Title' defaultValue={data.attributes.title || ''}
-        onChange={event => (data.attributes.title = event.target.value || '')}/>
-      <Form.Input label='Tag(s)' defaultValue={data.attributes.labels || ''}
-        onChange={event => (data.attributes.labels = event.target.value || '')}/>
+                  onChange={event => (data.attributes.title = event.target.value || '')}/>
       <Form.TextArea autoHeight label='Body' defaultValue={data.body || ''}
-        onChange={event => (data.body = event.target.value)}/>
+                     onChange={event => (data.body = event.target.value)}/>
+      <Accordion>
+        <Accordion.Title>
+          <Icon name='dropdown'/>
+          Tag(s)
+        </Accordion.Title>
+        <Accordion.Content>
+          {this.displayLabels()}
+          <Icon name='plus' color='teal' onClick={this.addLabel}/>
+        </Accordion.Content>
+      </Accordion>
+      <br/>
       <Button content='Save' color='green' onClick={this.save}/>
     </Form>;
   }
