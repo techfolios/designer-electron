e076aaa9898e944db3717fac3dc23511c21ac419
SoraZodia <sorazodia@users.noreply.github.com>
2017-08-02 01:21:13 -1000

    Add option to restore an deleted essay

--- app/src/components/MainMenu.jsx
+++ app/src/components/MainMenu.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Menu, Icon, Accordion, MenuItem } from 'semantic-ui-react';
+import { Menu, Icon, Accordion, MenuItem, Divider } from 'semantic-ui-react';
 
 import ProjectsMenu from './ProjectsMenu.jsx';
+import YAMLParser from '../utilities/yaml-parser';
 
 class MainMenu extends React.Component {
   constructor(props) {
@@ -12,6 +13,7 @@ class MainMenu extends React.Component {
       projects: props.projects,
       essayList: props.essays,
       essayCrawler: props.essayCrawler,
+      deletedEssay: undefined,
     };
 
     this.maxWidth = 16;
@@ -45,67 +47,78 @@ class MainMenu extends React.Component {
 
   renderBio(activeItem) {
     return (
-      <Accordion>
-        <Accordion.Title>
-          <Menu.Item name='bioMenu'>
-            <Icon name='dropdown'/>
-            <Icon name='user circle'/>
+        <Accordion>
+          <Accordion.Title>
+            <Menu.Item name='bioMenu'>
+              <Icon name='dropdown'/>
+              <Icon name='user circle'/>
               Bio
-          </Menu.Item>
-        </Accordion.Title>
-        <Accordion.Content>
-          <Menu.Item name='basicsSection' active={activeItem === 'basicsSection'} onClick={this.handleItemClick}>
+            </Menu.Item>
+          </Accordion.Title>
+          <Accordion.Content>
+            <Menu.Item name='basicsSection' active={activeItem === 'basicsSection'} onClick={this.handleItemClick}>
               Basics
-          </Menu.Item>
-          <Menu.Item name='profilesSection' active={activeItem === 'profilesSection'} onClick={this.handleItemClick}>
-            Profiles
-          </Menu.Item>
-          <Menu.Item name='interestsSection' active={activeItem === 'interestsSection'}
-            onClick={this.handleItemClick}>
+            </Menu.Item>
+            <Menu.Item name='profilesSection' active={activeItem === 'profilesSection'} onClick={this.handleItemClick}>
+              Profiles
+            </Menu.Item>
+            <Menu.Item name='interestsSection' active={activeItem === 'interestsSection'}
+                       onClick={this.handleItemClick}>
               Interests
-          </Menu.Item>
-          <Menu.Item name='skillsSection' active={activeItem === 'skillsSection'} onClick={this.handleItemClick}>
+            </Menu.Item>
+            <Menu.Item name='skillsSection' active={activeItem === 'skillsSection'} onClick={this.handleItemClick}>
               Skills
-          </Menu.Item>
-          <Menu.Item name='awardsSection' active={activeItem === 'awardsSection'} onClick={this.handleItemClick}>
+            </Menu.Item>
+            <Menu.Item name='awardsSection' active={activeItem === 'awardsSection'} onClick={this.handleItemClick}>
               Awards
-          </Menu.Item>
-          <Menu.Item name='educationSection' active={activeItem === 'educationSection'}
-            onClick={this.handleItemClick}>
+            </Menu.Item>
+            <Menu.Item name='educationSection' active={activeItem === 'educationSection'}
+                       onClick={this.handleItemClick}>
               Education
-          </Menu.Item>
-          <Menu.Item name='workSection' active={activeItem === 'workSection'} onClick={this.handleItemClick}>
+            </Menu.Item>
+            <Menu.Item name='workSection' active={activeItem === 'workSection'} onClick={this.handleItemClick}>
               Work
-          </Menu.Item>
-          <Menu.Item name='volunteerSection' active={activeItem === 'volunteerSection'}
-            onClick={this.handleItemClick}>
+            </Menu.Item>
+            <Menu.Item name='volunteerSection' active={activeItem === 'volunteerSection'}
+                       onClick={this.handleItemClick}>
               Volunteer
-          </Menu.Item>
-          <Menu.Item name='refSection' active={activeItem === 'refSection'} onClick={this.handleItemClick}>
+            </Menu.Item>
+            <Menu.Item name='refSection' active={activeItem === 'refSection'} onClick={this.handleItemClick}>
               References
-          </Menu.Item>
-        </Accordion.Content>
-      </Accordion>
+            </Menu.Item>
+          </Accordion.Content>
+        </Accordion>
     );
   }
 
   renderProjects(activeItem) {
     return (
-      <Menu.Item name='projects' active={activeItem === 'projects'} onClick={this.handleItemClick}>
-        <Icon name='cubes' />
-        Projects
-      </Menu.Item>
+        <Menu.Item name='projects' active={activeItem === 'projects'} onClick={this.handleItemClick}>
+          <Icon name='cubes'/>
+          Projects
+        </Menu.Item>
     );
   }
 
-  removeYAML(event, key, file, crawler, state) {
+  removeYAML(event, key, file, crawler, state, checkpoint) {
     event.preventDefault();
     const list = this.state;
+    if (checkpoint) list[checkpoint] = list[state][key];
     list[state] = list[state].filter((data, index) => index !== key);
     if (file) crawler.removeFile(file);
     console.log(list);
     this.setState(list);
     this.props.onMenuSelect('default');
+  }
+
+  restoreYAML(event, crawler, state, checkpoint) {
+    event.preventDefault();
+    const list = this.state;
+    if (!list[checkpoint]) return;
+    if (list[checkpoint].file) crawler.writeFile(list[checkpoint].file, YAMLParser.write(list[checkpoint]));
+    list[state].push(list[checkpoint]);
+    list[checkpoint] = undefined;
+    this.setState(list);
   }
 
   getShortenString(str) {
@@ -116,21 +129,20 @@ class MainMenu extends React.Component {
     return returnString;
   }
 
-  getYAML(files, crawler, state) {
+  getYAML(files, crawler, state, checkpoint) {
     const list = [];
     const { activeItem } = this.state;
     let key;
     files.forEach((data, index) => {
-      console.log(data);
       key = `${data.attributes.title}`;
       list.push(<Menu.Item name={key} key={key} active={activeItem === key}>
         {this.getShortenString(key)}
         <br/>
         <div>
           <Icon link size='big' name='edit' color='black'
-            onClick={event => this.handlePageChange(event, 'essays', data)}/>
+                onClick={event => this.handlePageChange(event, 'essays', data)}/>
           <Icon link size='big' name='remove' color='red'
-            onClick={event => this.removeYAML(event, index, data.file, crawler, state)}/>
+                onClick={event => this.removeYAML(event, index, data.file, crawler, state, checkpoint)}/>
         </div>
       </Menu.Item>);
     });
@@ -156,16 +168,22 @@ class MainMenu extends React.Component {
     return <Accordion as={MenuItem}>
       <Accordion.Title>
         <Menu.Item>
-          <Icon name='file text outline' />
-          <Icon name='dropdown' />
+          <Icon name='file text outline'/>
+          <Icon name='dropdown'/>
           Essays
         </Menu.Item>
       </Accordion.Title>
       <Accordion.Content>
-        {this.getYAML(this.state.essayList, this.state.essayCrawler, 'essayList')}
+        {this.getYAML(this.state.essayList, this.state.essayCrawler, 'essayList', 'deletedEssay')}
+        <Divider/>
         <Menu.Item>
-          <Icon link name='plus' color='green'
-            onClick={event => this.addYAML(event, this.state.essayList, this.state.essayCrawler, 'essayList')}/>
+          <span>
+            <Icon link size='big' name='plus' color='green'
+                  onClick={event => this.addYAML(event, this.state.essayList, this.state.essayCrawler, 'essayList')}/>
+            <Icon link={this.state.deletedEssay !== undefined} size='big' name='undo'
+                  disabled={!this.state.deletedEssay} color='teal' onClick={event =>
+                      this.restoreYAML(event, this.state.essayCrawler, 'essayList', 'deletedEssay')}/>
+          </span>
         </Menu.Item>
       </Accordion.Content>
     </Accordion>;
@@ -173,30 +191,30 @@ class MainMenu extends React.Component {
 
   renderUpload(activeItem) {
     return (
-      <Menu.Item name='upload' active={activeItem === 'upload'} onClick={this.handleUpload}>
-        <Icon name='upload'/>
+        <Menu.Item name='upload' active={activeItem === 'upload'} onClick={this.handleUpload}>
+          <Icon name='upload'/>
           Upload
-      </Menu.Item>
+        </Menu.Item>
     );
   }
 
   render() {
     const { activeItem, projects } = this.state;
     return (
-      <Menu vertical widths={this.maxWidth} fixed="left" icon='labeled' color="teal">
-        {this.renderBio(activeItem)}
+        <Menu vertical widths={this.maxWidth} fixed="left" icon='labeled' color="teal">
+          {this.renderBio(activeItem)}
 
-        <ProjectsMenu data={projects} setSelected={this.props.setSelected} saveProject={this.props.saveProject} />
+          <ProjectsMenu data={projects} setSelected={this.props.setSelected} saveProject={this.props.saveProject}/>
 
-        {this.renderEssays(activeItem)}
+          {this.renderEssays(activeItem)}
 
-        {this.renderUpload(activeItem)}
+          {this.renderUpload(activeItem)}
 
-        <Menu.Item name='addItem' active={activeItem === 'addItem'} onClick={this.handleItemClick}>
-          <Icon name='plus'/>
+          <Menu.Item name='addItem' active={activeItem === 'addItem'} onClick={this.handleItemClick}>
+            <Icon name='plus'/>
             Add Menu Item
-        </Menu.Item>
-      </Menu>
+          </Menu.Item>
+        </Menu>
     );
   }
 }
