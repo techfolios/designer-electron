3ebbd7d5ebc883ad34888701a98edaefcd261b74
chadmorita <chadmmm@hawaii.edu>
2017-08-04 02:37:39 -1000

    Fix introduced bugs

--- app/src/containers/settings/Settings.jsx
+++ app/src/containers/settings/Settings.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import { Button, Container, Icon } from 'semantic-ui-react';
 import Oauth from '../../utilities/Oauth';
 
-// const electronOauth2 = require('electron-oauth2');
-// const config = require('../../config.js');
-
 class Settings extends React.Component {
   constructor() {
     super();
@@ -14,9 +11,8 @@ class Settings extends React.Component {
     } else {
       this.state = { isLoggedIn: true };
     }
-
-    // this.logout = this.logout.bind(this);
-    // this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
+    this.login = this.login.bind(this);
   }
 
   handleItemClick(e, { name }) {
@@ -33,37 +29,6 @@ class Settings extends React.Component {
     Oauth.logout();
     this.setState({ isLoggedIn: false });
   }
-  // logout() {
-  //   window.localStorage.removeItem('githubtoken');
-  //   this.setState({ isLoggedIn: false });
-  // }
-
-  // login() {
-  //   let token;
-  //
-  //   const windowParams = {
-  //     alwaysOnTop: true,
-  //     autoHideMenuBar: true,
-  //     webPreferences: {
-  //       nodeIntegration: false,
-  //     },
-  //   };
-  //   const scopes = ['repo', 'user:email'];
-  //
-  //   const options = {
-  //     scope: scopes.join(' '),
-  //     accessType: 'online',
-  //   };
-  //
-  //   const myApiOauth = electronOauth2(config, windowParams);
-  //
-  //   myApiOauth.getAccessToken(options)
-  //     .then((t) => {
-  //       token = t;
-  //       window.localStorage.setItem('githubtoken', token.access_token);
-  //       this.setState({ isLoggedIn: true });
-  //     });
-  // }
 
   render() {
     const isLoggedIn = this.state.isLoggedIn;
