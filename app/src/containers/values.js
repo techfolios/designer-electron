d2e021f131cfd1376c9a3a0dd385db67e01cc30b
SoraZodia <sorazodia@users.noreply.github.com>
2017-08-16 01:39:58 -1000

    Better handle cases where acsii art can be mistaken as an html tag

--- app/src/containers/values.js
+++ app/src/containers/values.js
@@ -1,14 +1,30 @@
+import React from 'react';
+import domToReact from 'html-react-parser/lib/dom-to-react';
+
 const values = {
   dir: '',
   replace: (html) => {
     const json = html;
-    if (!html.attribs) return;
 
-    if (html.name === 'img') {
-      const imagePath = json.attribs.src.split(/[\\/]/);
-      imagePath[0] = values.dir;
-      json.attribs.src = imagePath.join('\\');
+    if (!html.attribs) return json;
+
+    switch (html.name) {
+      case 'html':
+      case 'head':
+      case 'title':
+      case 'body':
+        return <div>{domToReact(html.children, values)}</div>;
+      case 'img': {
+        const imagePath = json.attribs.src.split(/[\\/]/);
+        imagePath[0] = values.dir;
+        json.attribs.src = imagePath.join('\\');
+        break;
+      }
+      default:
+        return json;
     }
+
+    return json;
   },
 };
 
--- app/src/containers/yaml/YAMLDisplay.jsx
+++ app/src/containers/yaml/YAMLDisplay.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Menu } from 'semantic-ui-react';
 import preview from 'marked';
 import renderHTML from 'html-react-parser';
+import valilator from 'htmltidy2';
 
 import FileCrawler from '../../utilities/file-crawler';
 import ProjectEditor from '../project/components/ProjectEditor.jsx';
@@ -11,44 +12,51 @@ import values from '../values';
 class YAMLDisplay extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { page: 'edit' };
     this.directory = props.dir;
     this.crawler = new FileCrawler(this.directory);
+    this.editor = () => {
+      if (this.props.editor === 'project') {
+        return <ProjectEditor delete={this.props.delete}
+                              data={this.props.data} dir={this.directory} state={this.props.state}/>;
+      }
+      return <EssayEditor delete={this.props.delete}
+                          data={this.props.data} dir={this.directory} state={this.props.state}/>;
+    };
+    this.state = {
+      editor: this.editor(),
+      display: this.editor(),
+      active: 'edit',
+    };
 
     this.delete = this.props.delete;
-    this.getPage = this.getPage.bind(this);
     this.setPage = this.setPage.bind(this);
   }
 
   setPage(event, { name }) {
     event.preventDefault();
-    this.setState({ page: name });
-  }
-
-  getPage() {
-    let page = <p>404 Not Found</p>;
-    if (this.props.editor === 'project') {
-      page = <ProjectEditor delete={this.delete}
-                            data={this.props.data} dir={this.directory} state={this.props.state}/>;
-    } else {
-      page = <EssayEditor delete={this.delete}
-                   data={this.props.data} dir={this.directory} state={this.props.state}/>;
-    }
-    if (this.state.page === 'preview') {
-      console.log(renderHTML(preview(this.props.data.body), values));
-      page = renderHTML(preview(this.props.data.body), values);
-    }
-    return page;
+    const change = new Promise((res) => { res(name); });
+    const { editor } = this.state;
+    const data = this.props.data.body;
+    change.then((res) => {
+      if (res === 'edit') {
+        this.setState({ active: name, display: editor });
+      } else {
+        console.log(preview(data));
+        valilator.tidy(preview(data), (err, html) => {
+          this.setState({ active: name, display: renderHTML(html, values) });
+        });
+      }
+    });
   }
 
   render() {
-    const { page } = this.state;
+    const { active, display } = this.state;
     return <div>
       <Menu tabular>
-        <Menu.Item name='edit' active={page === 'edit'} onClick={this.setPage}/>
-        <Menu.Item name='preview' active={page === 'preview'} onClick={this.setPage}/>
+        <Menu.Item name='edit' active={active === 'edit'} onClick={this.setPage}/>
+        <Menu.Item name='preview' active={active === 'preview'} onClick={this.setPage}/>
       </Menu>
-      {this.getPage()}
+      {display}
     </div>;
   }
 }
