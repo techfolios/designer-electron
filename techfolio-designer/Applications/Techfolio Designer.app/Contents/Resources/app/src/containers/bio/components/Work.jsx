de70aaf56f6ce69189b1ebc9e73b8c9f070e362b
chadmorita <chadmmm@hawaii.edu>
2017-08-20 23:52:56 -1000

    Added MacOS application.

---
+++ techfolio-designer/Applications/Techfolio Designer.app/Contents/Resources/app/src/containers/bio/components/Work.jsx
@@ -0,0 +1,140 @@
+import React from 'react';
+import { Form, Icon, Segment } from 'semantic-ui-react';
+
+class Work extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: props.data,
+    };
+    this.add = this.add.bind(this);
+    this.remove = this.remove.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleHighlightChange = this.handleHighlightChange.bind(this);
+    this.addHighlight = this.addHighlight.bind(this);
+    this.removeHighlight = this.removeHighlight.bind(this);
+  }
+
+  handleChange(e, key, index) {
+    const data = this.state.data;
+    data[index][key] = e.target.value;
+    this.setState({ data });
+  }
+
+  handleHighlightChange(e, key, windex, hindex) {
+    const data = this.state.data;
+    data[windex][key][hindex] = e.target.value;
+    this.setState({ data });
+  }
+  addHighlight(e) {
+    const data = this.state.data;
+    const index = e.currentTarget.getAttribute('data-index');
+    data[index].highlights.push('');
+    this.setState({ data });
+  }
+  removeHighlight(e) {
+    const data = this.state.data;
+    const index = e.currentTarget.getAttribute('data-index');
+    const hindex = e.currentTarget.getAttribute('data-hindex');
+    data[index].highlights.splice(hindex, 1);
+    console.log(data[index].highlights);
+    this.setState({ data });
+  }
+
+  add() {
+    const data = this.state.data;
+    data.push({
+      company: '',
+      position: '',
+      website: '',
+      startDate: '',
+      endDate: '',
+      summary: '',
+      highlights: [''],
+    });
+    this.setState({ data });
+  }
+
+  remove() {
+    const data = this.state.data;
+    data.pop();
+    this.setState({ data });
+  }
+
+  render() {
+    return <div>
+      {this.state.data.map((work, index) => <Segment basic key={index}>
+        <Form.Group>
+          <Form.Input
+            width={8}
+            label={<span data-position="bottom center" data-tooltip={work.company}>
+              <Icon color="teal" name={`laptop ${work.company}`} />
+              Organization
+            </span>}
+            defaultValue={work.company}
+            placeholder='Company'
+            onChange={e => this.handleChange(e, 'company', index)} />
+          <Form.Input
+            width={8}
+            label='Position'
+            defaultValue={work.position}
+            placeholder="Position"
+            onChange={e => this.handleChange(e, 'position', index)} />
+        </Form.Group>
+        <Form.Group>
+          <Form.Input
+            width={4}
+            label='End Date'
+            defaultValue={work.endDate}
+            placeholder="01/01/9999"
+            onChange={e => this.handleChange(e, 'endDate', index)} />
+          <Form.Input
+            width={4}
+            label='Start Date'
+            defaultValue={work.startDate}
+            placeholder='01/01/1970'
+            onChange={e => this.handleChange(e, 'startDate', index)} />
+          <Form.Input
+            width={8}
+            label='Website'
+            defaultValue={work.website}
+            placeholder="www.exampleco.com"
+            onChange={e => this.handleChange(e, 'website', index)}
+          />
+        </Form.Group>
+        <Form.Input
+          label='Summary'
+          defaultValue={work.summary}
+          placeholder="Lead developer for weather sensing project"
+          onChange={e => this.handleChange(e, 'summary', index)}
+        />
+        {work.highlights.map((highlight, hindex) =>
+          <div key={`div:${hindex}`}>
+            <Form.Input
+              className="highlight"
+              key={hindex}
+              label='Highlight'
+              value={highlight}
+              placeholder="Design firmware for distributed weather sensor network"
+              onChange={e => this.handleHighlightChange(e, 'highlights', index, hindex)}
+            />
+            <Icon key={`remove:${hindex}`} data-index={index} data-hindex={hindex} link name="minus"
+                  onClick={this.removeHighlight}></Icon>
+            {(work.highlights.length - 1 === hindex) &&
+              <Icon data-index={index} link name="plus" color="teal" onClick={this.addHighlight}></Icon>
+            }
+          </div>)}
+          {(work.highlights.length === 0) &&
+            <span data-position="bottom center" data-tooltip="Add a highlight">
+              <Icon data-index={index} link name="plus" color="teal" onClick={this.addHighlight}></Icon>
+            </span>
+          }
+      </Segment>)
+      }
+      <Icon link name="minus" onClick={this.remove} ></Icon>
+      <Icon link name="plus" color="teal" onClick={this.add} ></Icon>
+    </div>;
+  }
+}
+
+export default Work;
---
+++ techfolio-designer/Applications/Techfolio Designer.app/Contents/Resources/app/src/io.js
@@ -0,0 +1,336 @@
+import FS from 'fs';
+import OS from 'os';
+import Path from 'path';
+import Git from 'nodegit';
+import fse from 'fs-extra';
+import request from 'superagent';
+
+import FileCrawler from './utilities/file-crawler';
+import values from './containers/values';
+
+class IO {
+  constructor() {
+    this.templateURL = 'https://github.com/techfolios/template';
+    this.localURL = Path.resolve(OS.homedir(), '.techfolios');
+    values.dir = this.localURL;
+
+    this.bioURL = Path.resolve(this.localURL, '_data/bio.json');
+    this.projectsURL = Path.resolve(this.localURL, 'projects');
+    this.essaysURL = Path.resolve(this.localURL, 'essays');
+    this.imagesURL = Path.resolve(this.localURL, 'images');
+  }
+
+  getUsername() {
+    return request('GET', `https://api.github.com/user?access_token=${this.accessToken}`)
+      .then((res) => {
+        this.username = JSON.parse(res.text).login;
+        return this.username;
+      });
+  }
+
+  init() {
+    this.accessToken = window.localStorage.getItem('githubtoken');
+    return new Promise((res) => {
+      this.getUsername().then(() => {
+        this.remoteURL = `https://github.com/${this.username}/${this.username}.github.io`;
+        this.hasLocal()
+          .then((hasLocal) => {
+            if (hasLocal) {
+              res('Local techfolio found');
+            } else {
+              this.hasRemote()
+                .then((hasRemote) => {
+                  if (hasRemote) {
+                    this.cloneUserRemote().then(() => {
+                      res('Cloned remote techfolio');
+                    });
+                  } else {
+                    this.cloneTechfoliosTemplate().then(() => {
+                      res('Cloned techfolios template');
+                    });
+                  }
+                });
+            }
+          });
+      });
+    });
+  }
+
+  hasLocal() {
+    return new Promise((res, rej) => {
+      FS.mkdir(this.localURL, (err) => {
+        if (!err) {
+          res(false);
+        } else if (err.code === 'EEXIST') {
+          res(true);
+        } else {
+          rej(err);
+        }
+      });
+    });
+  }
+
+  // Helper function used to get the string of the user's techfolio remote repo name
+  getRemoteRepoString() {
+    let remoteRepoString;
+    return new Promise((res) => {
+      this.getUsername()
+        .then((username) => {
+          remoteRepoString = `${username}.github.io`;
+          res(remoteRepoString);
+        });
+    });
+  }
+
+  /*
+  added "this." in front of res to placate ESLint - is this a correct fix?
+   */
+  hasRemote() {
+    return request('GET',
+      `https://api.github.com/repos/${this.username}/${this.username}.github.io?access_token=${this.accessToken}`)
+      .then((res) => {
+        if (res.body.id) {
+          return true;
+        }
+        return false;
+      }, (err) => {
+        console.err(err);
+      });
+  }
+
+  cloneUserRemote() {
+    return new Promise((res, rej) => {
+      Git.Clone(this.remoteURL, this.localURL)
+        .then((repo) => {
+          res(repo.mergeBranches('master', 'origin/master'));
+        })
+        .catch((err) => { rej(err); });
+    });
+  }
+
+  cloneTechfoliosTemplate() {
+    return new Promise((res, rej) => {
+      Git.Clone(this.templateURL, this.localURL)
+        .then((repo) => {
+          res(repo.mergeBranches('master', 'origin/master'));
+        })
+        .catch((err) => { rej(err); });
+    });
+  }
+
+  push() {
+    let repo;
+    let index;
+    let oid;
+    let remote;
+
+    return new Promise((res, rej) => {
+      // Open the repository.
+      console.log(this.remoteURL);
+      Git.Repository.open(this.localURL)
+        // Get the index.
+        .then((repoResult) => {
+          repo = repoResult;
+          return repoResult.refreshIndex();
+        })
+        // Assign the index to indexResult.
+        .then((indexResult) => {
+          index = indexResult;
+        })
+        // Add all files to the index.
+        .then(() => index.addAll())
+        // Write all files to index.
+        .then(() => index.write())
+        .then(() => index.writeTree())
+        .then((oidResult) => {
+          oid = oidResult;
+          return Git.Reference.nameToId(repo, 'HEAD');
+        })
+        .then((head) => {
+          console.log();
+          return repo.getCommit(head);
+        })
+        // Create a signature and commit
+        .then((parent) => {
+          const author = Git.Signature.default(repo);
+          return repo.createCommit('HEAD', author, author, 'Update from Techfolio Designer', oid, [parent]);
+        })
+        .then(() => repo.getRemote('origin'))
+        .then((remoteResult) => {
+          remote = remoteResult;
+          console.log('pushing to remote URL');
+          console.log(remoteResult);
+          console.log(remote);
+          console.log(this.remoteURL);
+          return remote.push(
+            ['refs/heads/master:refs/heads/master'],
+            {
+              callbacks: {
+                certificateCheck: () => 1,
+                credentials: () => Git.Cred.userpassPlaintextNew(this.accessToken, 'x-oauth-basic'),
+              },
+            });
+        })
+        // Catch any errors.
+        .catch((err) => { rej(err); })
+        .done(() => {
+          res('successfully pushed');
+        });
+    });
+  }
+
+  pull() {
+    let repo;
+
+    return new Promise((res, rej) => {
+      Git.Repository.open(this.localURL)
+        .then((repoResult) => {
+          repo = repoResult;
+
+          return repo.fetchAll({
+            callbacks: {
+              certificateCheck: () => 1,
+              credentials: () => Git.Cred.userpassPlaintextNew(this.accessToken, 'x-oauth-basic'),
+            },
+          });
+        })
+        .then(() => repo.mergeBranches('master', 'origin/master'))
+        .catch((err) => { rej(err); })
+        .done(() => {
+          const path = this.bioURL;
+          FS.readFile(path, (err, data) => {
+            if (err) {
+              rej(err);
+            }
+            res(JSON.parse(data));
+          });
+        });
+    });
+  }
+
+  loadBio() {
+    return new Promise((res, rej) => {
+      const path = this.bioURL;
+      FS.readFile(path, (err, data) => {
+        if (err) {
+          rej(err);
+        }
+        res(JSON.parse(data));
+      });
+    });
+  }
+
+  writeBio(data) {
+    return new Promise((res, rej) => {
+      FS.writeFile(this.bioURL, JSON.stringify(data, null, 2), (err) => {
+        if (err) {
+          rej(err);
+        }
+        let repo;
+        let index;
+        let oid;
+        Git.Repository.open(this.localURL)
+          .then((repoResult) => {
+            repo = repoResult;
+            return fse.ensureDir(this.localURL);
+          })
+          .then(() => repo.refreshIndex())
+          .then((indexResult) => {
+            index = indexResult;
+          })
+          .then(() => index.addAll())
+          .then(() => index.write())
+          .then(() => index.writeTree())
+          .then((oidResult) => {
+            oid = oidResult;
+            return Git.Reference.nameToId(repo, 'HEAD');
+          })
+          .then(head => repo.getCommit(head))
+          .then((parent) => {
+            const author = Git.Signature.now('Techfolios', 'me@techfolios.com');
+            const committer = Git.Signature.now('Techfolios', 'me@techfolios.com');
+            res(true);
+            return repo.createCommit('HEAD', author, committer, 'Update Techfolio', oid, [parent]);
+          })
+          .catch((error) => {
+            console.error(`commitBio: ${error}`);
+            rej(error);
+          });
+      });
+    });
+  }
+
+  loadProjects() {
+    return new Promise((res) => {
+      const list = {};
+      console.log(this.projectsURL);
+      const crawler = new FileCrawler(this.projectsURL);
+      list.projects = crawler.getYAML();
+      list.crawler = crawler;
+      res(list);
+    });
+  }
+
+  loadEssays() {
+    return new Promise((res) => {
+      const list = {};
+      console.log(this.essaysURL);
+      const crawler = new FileCrawler(this.essaysURL);
+      list.essays = crawler.getYAML();
+      list.crawler = crawler;
+      res(list);
+    });
+  }
+
+  loadImages() {
+    return new Promise((res, rej) => {
+      FS.readdir(this.imagesURL, (err, data) => {
+        if (err) {
+          rej(err);
+        } else {
+          const result = data.map(image => Path.resolve(this.imagesURL, image));
+          res(result);
+        }
+      });
+    });
+  }
+
+  importImage(url) {
+    return new Promise((res) => {
+      const imageName = Path.basename(url);
+      console.log(this.imagesURL);
+      const newImagePath = Path.resolve(this.imagesURL, imageName);
+      const image = FS.readFileSync(url);
+      FS.writeFileSync(newImagePath, image);
+      res(newImagePath);
+    });
+  }
+
+  saveImage(name, data) {
+    return new Promise((res) => {
+      console.log(this);
+      console.log([name, data]);
+      res(true);
+    });
+  }
+
+  removeImage(url) {
+    return new Promise((res, rej) => {
+      const imagePath = Path.resolve(this.imagesURL, url);
+      console.log(imagePath);
+      FS.unlink(imagePath, (err) => {
+        if (err) {
+          rej(err);
+        } else {
+          res(true);
+        }
+      });
+    });
+  }
+
+  getLocalFolder() {
+    return this.localURL;
+  }
+}
+
+export default IO;
