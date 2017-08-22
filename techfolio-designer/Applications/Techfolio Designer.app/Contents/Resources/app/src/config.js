961e202794b20923506b89b0a6674c5d9fa389ac
chadmorita <chadmmm@hawaii.edu>
2017-08-22 11:48:22 -1000

    Delete installers

--- techfolio-designer/Applications/Techfolio Designer.app/Contents/Resources/app/src/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  clientId: Buffer('NDdiNWQwNWM2ZDc5YmFmMmE1NTg=', 'base64').toString(),
-  clientSecret: Buffer('Y2U1YzU4NjUxMDU4MGQ5OTFlMWQ2YTgyZTYwM2ZhOTkyMjM4ZmYyMQ==', 'base64').toString(),
-  authorizationUrl: 'https://github.com/login/oauth/authorize',
-  tokenUrl: 'https://github.com/login/oauth/access_token',
-  useBasicAuthorizationHeader: false,
-  redirectUri: 'https://techfolios.github.io/designer-electron/',
-};
